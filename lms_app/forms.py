c2e43e0503be05381ee1c904e351cad1b7a1dab5
medalysai05 <163214139+medalysai05@users.noreply.github.com>
2024-03-16 14:25:46 +0100

    Add files via upload

---
+++ lms_app/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Book , Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 
+                  'author',
+                  'photo_book',
+                  'photo_author',
+                  'pages',
+                  'price',
+                  'rental_price_day',
+                  'rental_period',
+                  'status',
+                  'category',
+                  ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'photo_book': forms.FileInput(attrs={'class': 'form-control-file'}),
+            'photo_author': forms.FileInput(attrs={'class': 'form-control-file'}),
+            'pages': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'rental_price_day': forms.NumberInput(attrs={'class': 'form-control'}),
+            'rental_period': forms.NumberInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+        #'__all__'
---
+++ lms_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Category, Book
+from .forms import BookForm , CategoryForm
+
+def index(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+         
+        add_cat =  CategoryForm(request.POST)
+        if add_cat.is_valid():
+            add_cat.save()
+            # Rediriger l'utilisateur ou faire toute autre action nécessaire
+    else:
+        form = BookForm()
+    
+    context = {
+        'category': Category.objects.all(),
+        'books': Book.objects.all(),
+        'form': form,
+        'formcat': CategoryForm(),
+    }
+    return render(request, 'pages/index.html', context)
+
+def books(request):
+    context = {
+        'category': Category.objects.all(),
+        'books': Book.objects.all(),
+    }
+    return render(request, 'pages/books.html', context)
+
+#from .models import *   categories = Category.objects.all()
